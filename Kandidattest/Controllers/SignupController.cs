0ace5319a0866d74d8266a70760db574e2bfe2bf
JeremiahUy <jery_3000@hotmail.com>
2018-12-03 12:15:57 +0100

    2nd commit, imported project

---
+++ Kandidattest/Controllers/SignupController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Kandidattest.Controllers
+{
+    public class SignupController : Controller
+    {
+        //
+        // GET: /Signup/
+        public ActionResult Index()
+        {          
+            return View();
+        }
+    }
+}
---
+++ Kandidattest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kandidattest.Startup))]
+namespace Kandidattest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
