275972e2f89bc146fc98db97322c164eda71a5c1
JeremiahUy <jery_3000@hotmail.com>
2018-12-03 12:57:03 +0100

    navbar er laget, starter med form

---
+++ Kandidattest/Controllers/OppgaveController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kandidattest.Controllers
+{
+    public class OppgaveController : Controller
+    {
+        // GET: Oppgave
+        public ActionResult oppgave()
+        {
+            return View();
+        }
+    }
+}
---
+++ Kandidattest/Models/VMsignup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Kandidattest.Models
+{
+    public class VMsignup
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Fornavn { get; set; }
+        [Required]
+        public string Etternavn { get; set; }
+        [Required]
+        public int Fødselsnummer { get; set; }
+
+    }
+}
